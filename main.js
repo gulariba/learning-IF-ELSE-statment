14c1c82f027cdc6a3b3da8379ec8c8d78dc6e4be
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2024-05-15 13:14:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+let weather = 45;
+if (weather > 45) {
+    console.log("weather is hot");
+}
+else if (weather < 40) {
+    console.log("weather is cold");
+}
+else {
+    console.log("weather is normal");
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+let weather = 45;
+  if (weather > 45)
+    {console.log("weather is hot");}
+else  if (weather < 40)
+    {console.log("weather is cold")
+}
+else {
+    console.log("weather is normal")
+}
